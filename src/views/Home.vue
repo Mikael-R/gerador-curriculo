013b6ee5693226c623ce9ab126c40b277d648ad9
Mikael-R <mikarg9@gmail.com>
2025-04-19 02:51:25 -0300

    feat: implement form components with validation using vee-validate and zod

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1 class="text-center text-2xl font-semibold"> Gerador de currículo </h1>
-    <Form />
+    <h1 class="text-center text-2xl font-semibold my-3"> Gerador de currículo </h1>
+    <Form class="max-w-md mx-auto my-5"/>
   </div>
 </template>
+
+<script setup lang="ts">
+import Form from '@/components/Form.vue'
+</script>
