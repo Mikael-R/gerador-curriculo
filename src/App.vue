a35be897b398c95610381eeb9a4574e851a983c3
Mikael-R <mikarg9@gmail.com>
2025-04-19 17:59:08 -0300

    feat: add ToggleTheme component for light/dark mode toggle and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <main class="min-h-screen min-w-screen relative p-4">
+    <ToggleTheme />
     <RouterView />
   </main>
 </template>
+
+<script setup lang="ts">
+import { ToggleTheme } from '@/components/toggle-theme'
+</script>
---
+++ src/components/toggle-theme/ToggleTheme.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Icon } from '@iconify/vue'
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode({ disableTransition: false })
+
+const toggleMode = () => {
+  mode.value = mode.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <Button @click="toggleMode" variant="outline" size="icon" class="absolute top-4 right-4">
+    <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+  </Button>
+</template>
