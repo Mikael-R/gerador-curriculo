a48d9c1ab7870027ba489a65ec8f5b9de52d37f2
Mikael-R <mikarg9@gmail.com>
2025-04-19 03:54:40 -0300

    refactor: remove unused dependencies and clean up stylesheets

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import Toast from "vue-toastification";
 
 import "@fontsource/poppins/100.css";
 import "@fontsource/poppins/300.css";
@@ -12,13 +11,8 @@ import App from "@/App.vue";
 import router from "@/router";
 
 import "@/assets/main.css";
-import "@/assets/swal.css";
-import "@/assets/vue-multiselect.css";
-import "vue-toastification/dist/index.css";
-import "tippy.js/dist/tippy.css";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(Toast);
 app.mount("#app");
