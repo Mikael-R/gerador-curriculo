c410658bc298c75bca20d36d52e2167202b7b5e7
Mikael-R <mikarg9@gmail.com>
2025-04-19 17:45:13 -0300

    feat: add loading state support to button component with spinner

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -2,12 +2,14 @@
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
+import { Loader2 } from 'lucide-vue-next'
 import { type ButtonVariants, buttonVariants } from '.'
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  loading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -20,7 +22,9 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="loading"
   >
+    <Loader2 v-if="loading" class="w-4 h-4 mr-2 animate-spin" />
     <slot />
   </Primitive>
 </template>
