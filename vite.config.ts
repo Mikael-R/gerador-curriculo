e30db02d4759b87faaf2ba956060c5c59949d4ae
Mikael-R <mikarg9@gmail.com>
2025-06-08 22:44:28 -0300

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // VitePWA({ registerType: 'autoUpdate' })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,9 +12,9 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: 'esbuild',
+    minify: 'esbuild'
   },
   server: {
-    allowedHosts: true,
+    allowedHosts: true
   }
 })
